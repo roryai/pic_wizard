88e4ba3cf7e4cd22071eea2b4fa75a098298e83d
Rory <rory.aitchison@gmail.com>
2018-11-24 08:06:52 +0000

    copies files to directories by month

---
+++ lib/pic_wizard.rb
@@ -0,0 +1,104 @@
+require 'fileutils'
+
+class PicWizard
+
+# fix these
+  attr_accessor :source, :destination_root, :files, :months, :log, :copy_count
+
+  def initialize
+    @source = '/Users/rory/code/pic_wizard/sample_files'
+    @destination_root = '/Users/rory/code/pic_wizard/test_dest'
+    @files = []
+    @months = {
+      1 => 'January',
+      2 => 'February',
+      3 => 'March',
+      4 => 'April',
+      5 => 'May',
+      6 => 'June',
+      7 => 'July',
+      8 => 'August',
+      9 => 'September',
+      10 => 'October',
+      11 => 'November',
+      12 => 'December'}
+      @log = ''
+      @copy_count = 0
+  end
+
+  def process
+    collect_file_names
+    self.files.each do |file_name|
+      create_directory_if_not_exists(file_name)
+      copy_file(file_name)
+    end
+    puts self.log
+    puts self.copy_count
+  end
+
+  def collect_file_names
+    Dir.chdir(self.source)
+    self.files = Dir.glob('**/*.{HEIC,PNG,mov,mp4,jpg}')
+  end
+
+  def create_directory_if_not_exists(file_name)
+    source_file_path = self.source + '/' + file_name
+    time = get_ctime(source_file_path)
+    year_string = get_year(time)
+    month_string = get_month(time)
+    dest_year = destination_dir_year(year_string)
+    dest_month = destination_dir_month(dest_year, month_string)
+
+    if (!Dir.exists?(dest_year))
+      Dir.mkdir(dest_year)
+      if (!Dir.exists?(dest_month))
+        Dir.mkdir(dest_month)
+      end
+    end
+  end
+
+  def copy_file(file_name)
+    full_dest_path = full_dest_path(file_name)
+    full_source_path = self.source + '/' + file_name
+    if (!File.exists?(full_dest_path))
+      FileUtils.cp(full_source_path, full_dest_path)
+    end
+    self.log += "Copied #{full_source_path} to #{full_dest_path}\n"
+    self.copy_count += 1
+  end
+
+  def create_directory(dir)
+    Dir.mkdir(dir)
+  end
+
+  def get_ctime(file_path)
+    # perhaps use .utime here
+    File.ctime(file_path)
+  end
+
+  def get_month(time)
+    a = self.months[time.month]
+    puts a
+    a
+  end
+
+  def get_year(time)
+    time.year.to_s
+  end
+
+  def destination_dir_year(year_string)
+    self.destination_root + '/' + year_string
+  end
+
+  def destination_dir_month(year_path, month_string)
+    year_path + '/' + month_string
+  end
+
+  def full_dest_path(file_name)
+    time = get_ctime(self.source + '/' + file_name)
+    self.destination_root + '/' + get_year(time) + '/' + get_month(time) + '/' + file_name
+  end
+
+end
+wiz = PicWizard.new
+wiz.process
---
+++ spec/pic_wizard_spec.rb
@@ -0,0 +1,100 @@
+require_relative '../lib/pic_wizard'
+require 'fileutils'
+
+describe PicWizard do
+
+  after(:each) do
+    dirs = []
+    dir1 = '/Users/rory/code/pic_wizard/test_dest/dir'
+    dir2 = '/Users/rory/code/pic_wizard/test_dest/2018/November'
+    dir3 = '/Users/rory/code/pic_wizard/test_dest/2018'
+    dirs << dir1
+    dirs << dir2
+    dirs << dir3
+    dirs.each do |dir|
+      if (Dir.exists?(dir))
+        FileUtils.rm_r(dir)
+      end
+    end
+  end
+
+  describe '.collect_file_names' do
+
+    it 'can find HEIC files' do
+      subject.collect_file_names
+      expect(subject.files[0]).to eql('sample_files/IMG_0603.HEIC')
+    end
+
+    it 'can find all relevant file types, excludes irrelevant file types' do
+      expect(subject.files.include?('sample_files/IMG_0603.HEIC')).to be false
+      expect(subject.files.include?('sample_files/IMG_0965.PNG')).to be false
+      expect(subject.files.include?('sample_files/IMG_0969.mov')).to be false
+      expect(subject.files.include?('sample_files/ScreenRecording.mp4')).to be false
+      expect(subject.files.include?('sample_files/camphoto_341603450 (2).jpg')).to be false
+      subject.collect_file_names
+      expect(subject.files.include?('sample_files/IMG_0603.HEIC')).to be true
+      expect(subject.files.include?('sample_files/IMG_0965.PNG')).to be true
+      expect(subject.files.include?('sample_files/IMG_0969.mov')).to be true
+      expect(subject.files.include?('sample_files/ScreenRecording.mp4')).to be true
+      expect(subject.files.include?('sample_files/camphoto_341603450 (2).jpg')).to be true
+      expect(subject.files.include?('sample_files/no_copy.txt')).to be false
+    end
+  end
+
+  describe '.create_directory' do
+
+    it 'creates a test directory' do
+      expect(Dir.exists?('/Users/rory/code/pic_wizard/test_dest/dir')).to be false
+      subject.create_directory('/Users/rory/code/pic_wizard/test_dest/dir')
+      expect(Dir.exists?('/Users/rory/code/pic_wizard/test_dest/dir')).to be true
+    end
+  end
+
+  describe '.get_ctime' do
+
+    it 'can get the month from a file' do
+      subject.collect_file_names
+      time = subject.get_ctime(subject.files[0])
+      expect(subject.get_month(time)).to eql('November')
+    end
+
+    it 'can get the year from a file' do
+      subject.collect_file_names
+      time = subject.get_ctime(subject.files[0])
+      expect(subject.get_year(time)).to eql('2018')
+    end
+  end
+
+  describe '.create_directory_if_not_exists' do
+
+    it "create the year and month directories if they don't exist" do
+      year_dir = '/Users/rory/code/pic_wizard/test_dest/2018'
+      month_dir = '/Users/rory/code/pic_wizard/test_dest/2018/November'
+      expect(Dir.exists?(year_dir)).to be false
+      expect(Dir.exists?(month_dir)).to be false
+      subject.create_directory_if_not_exists('IMG_0965.PNG')
+      expect(Dir.exists?(year_dir)).to be true
+      expect(Dir.exists?(month_dir)).to be true
+    end
+  end
+
+  describe '.copy_file' do
+
+    it 'copies file to source' do
+      destination_root = '/Users/rory/code/pic_wizard/test_dest'
+      file_name = 'IMG_0965.PNG'
+      subject.collect_file_names
+      subject.create_directory_if_not_exists(file_name)
+      subject.copy_file(file_name)
+      expect(File.exists?(destination_root + '/' + file_name))
+    end
+  end
+
+  describe '.process' do
+
+    it 'creates all directories and copies all files' do
+
+    end
+  end
+
+end
